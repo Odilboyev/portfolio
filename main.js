6e97f102252ea70a05a238500ff3079500b531e7
Odilboyev <samandarodilboyev@gmail.com>
2021-01-01 12:56:31 +0500

    files

---
+++ main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+  
+
+  $(window).scroll(function () {
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 80) {
+      $('#navbar').addClass('bg-blur');
+    }
+    if ($(window).scrollTop() < 81) {
+      $('#navbar').removeClass('bg-blur');
+    }
+  });
+
+
+  
+
+});
+
+
+
+
